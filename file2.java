ad79b184b7f76d82a551ce087e138c259418bd3e
kurmareshma <kurmareshma123@gmail.com>
2025-09-05 18:51:58 +0530

    Update file2.java

--- file2.java
+++ file2.java
@@ -3,6 +3,6 @@ public class file2
     public static void main(String[] args)
     {
       System.out.println("Hello world");
-      System.out.println("hello india");
+      System.out.println("hello india"); 
     }
   }
